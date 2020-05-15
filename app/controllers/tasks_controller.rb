67902482e4f27a96cf5d53120b3ea29cee45fe9f
Mesi21 <mse212002@gmail.com>
2020-05-15 17:56:28 +0300

    :art: add conditional statement to index action and view file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,9 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order("created_at DESC")
+    if user_signed_in?
+      @tasks = Task.where(:user_id => current_user.id).order("created_at DESC")
+    end
   end
 
   def new
